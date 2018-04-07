c8017bd87ce29c9aafd3da34183c9f55a40fded0
newmannhu <newmannhu@qq.com>
2018-04-07 22:59:25 +0800

    20180407 微信

--- proxy.conf.js
+++ proxy.conf.js
@@ -5,7 +5,7 @@ const proxyConfig = [
     context: '/api',
     // pathRewrite: { '^/api': '' },
     // target: 'http://localhost:8110',
-    target:"http://127.0.0.1:8110",
+    target:"http://127.0.0.1:8120",
     changeOrigin: true,
     secure: false,
     logLevel: "debug"
